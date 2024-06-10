c3161f003559c65050c7a5f8bca14b50f7ab503a
Cheng Yu Liu <162671446+lcy1310@users.noreply.github.com>
2024-06-10 20:57:08 +0800

    Update BgObj.java
    same as main (published at 6/9)

--- BgObj.java
+++ BgObj.java
@@ -1,22 +1,27 @@
-import java.awt.*;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 
-//Background object's settings and drawing
-class BgObj extends GameObj {
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
 
+
+
+public class BgObj extends GameObj {
     public BgObj() {
         super();
     }
-
     public BgObj(Image img, int x, int y, double speed) {
         super(img,x,y,speed);
     }
 
-    //Implement a scrolling effect 
     @Override
     public void paintSelf(Graphics gImage) {
         super.paintSelf(gImage);
         y += speed;
-
         if (y >= 0) {
             y = -435;
         }
