a651e5f1c462cb13f090632cf1d18cdc7e30681c
Cheng Yu Liu <162671446+lcy1310@users.noreply.github.com>
2024-06-10 21:07:07 +0800

    Update PlaneObj.java
    same as main (published at 6/9)

--- PlaneObj.java
+++ PlaneObj.java
@@ -1,22 +1,27 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
 
-//Plane moves with mouse. Game over while you collide with boss
-class PlaneObj extends GameObj{
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
+public class PlaneObj extends GameObj{
     @Override
     public Image getImage() {
         return super.getImage();
     }
-
     public PlaneObj() {
         super();
     }
-
-    //Your plane moves with your mouse
     public PlaneObj(Image img,int x,int y,int width,int height,double speed,Planewar frame) {
         super(img,x,y,width,height,speed,frame);
-
         this.frame.addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseMoved (MouseEvent e) {
@@ -25,17 +30,13 @@ class PlaneObj extends GameObj{
             }
         });
     }
-
-    //If you collide with boss, gameover
     @Override
     public void paintSelf(Graphics gImage) {
         super.paintSelf(gImage);
-
         if (this.frame.bossObj != null && this.getRec().intersects(this.frame.bossObj.getRec())) {
             Planewar.currentState = Planewar.GameState.GAMEOVER;
         }
     }
-
     public Rectangle getRec() {
         return new Rectangle(x,y,width,height);
     }
