4dd9591d8a541ff578b568bfecc9d3c7051794de
Cheng Yu Liu <162671446+lcy1310@users.noreply.github.com>
2024-06-10 20:58:51 +0800

    Update BossObj.java
    health bar bug fixed
    boss's move range fixed with width

--- BossObj.java
+++ BossObj.java
@@ -1,45 +1,49 @@
-import java.awt.*;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
 
-//Boss moving type, detects collisions, and draws health bar.
-class BossObj extends GameObj {
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class BossObj extends GameObj {
+    int basiclife = 10;
     int life = 10;
-
-    public BossObj(Image img,int x,int y,int width,int height,double speed,Planewar frame) {
+    int gameLevel = 0;
+    public BossObj(Image img,int x,int y,int width,int height,double speed, int gameLevel,Planewar frame) {
         super(img,x,y,width,height,speed,frame);
+        this.gameLevel = gameLevel;
+        this.life = (gameLevel + 1) * 10;
+        this.basiclife = (gameLevel + 1) * 10;
     }
-
-
     @Override
     public void paintSelf(Graphics gImage) {
         super.paintSelf(gImage);
-
-        //Boss moves left and right
-        if (x > 550 || x < -50) {
+        if (x > Planewar.width-100 || x < -50) {
             speed = -speed;
         }
         x += speed;
-
         for (ShellObj shellObj : GameUtil.shellObjList) {
-
             if (this.getRec().intersects(shellObj.getRec())) {
                 shellObj.setX(-100);
                 shellObj.setY(100);
-                GameUtil.removeList.add(shellObj); //The shell intersects boss should moved out of map and added to removeList
-                life--; //If your shell intersects boss, boss life -1 
+                GameUtil.removeList.add(shellObj);
+                life--;
             }
-
             if (life <= 0) {
-                Planewar.currentState = Planewar.GameState.VICTORY;
+                GameUtil.removeList.add(this);
+                Planewar.bossAlive = false;
+                if(gameLevel == 2) Planewar.currentState = Planewar.GameState.VICTORY;
             }
         }
-
-        //Painting health bar for boss
         gImage.setColor(Color.white);
         gImage.fillRect(20,40,100,10);
         gImage.setColor(Color.red);
-        gImage.fillRect(20,40,(life*100)/10,10);
+        gImage.fillRect(20,40,(life*(100))/basiclife,10);
     }
-
     public Rectangle getRec() {
         return new Rectangle(x,y,width,height);
     }
