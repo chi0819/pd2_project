b972acf61da978ad1930c2e8d14e5249ad246e37
ethan8190 <eason8190@gmail.com>
2024-06-13 02:11:00 +0800

    Resolved conflicts

---
+++ PlayerPlaneObj.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
+
+class PlayerPlaneObj extends GameObj {
+    private int health;
+    public int id;
+
+    public PlayerPlaneObj(Image img, int x, int y, int width, int height, double speed, Planewar frame, int health,
+            int id) {
+        super(img, x, y, width, height, speed, frame);
+        this.health = health;
+        this.id = id;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void reduceHealth(int amount) {
+        health -= amount;
+        if (health <= 0) {
+            health = 0;
+        }
+    }
+
+    @Override
+    public void paintSelf(Graphics gImage) {
+        Graphics2D g2d = (Graphics2D) gImage;
+        AffineTransform old = g2d.getTransform();
+
+        // 將圖片旋轉180度
+        if (this.id == 1) {
+            g2d.rotate(Math.toRadians(180), x + width / 2, y + height / 2);
+        }
+        g2d.drawImage(img, x, y, width, height, null);
+
+        g2d.setTransform(old); // 恢復原來的變換
+    }
+
+    public void moveUp() {
+        // 確保玩家不會超過視窗上邊界，並根據玩家ID限制區域
+        if (y - speed >= 0 && (id == 1 || y - speed >= PVPmode.height / 2)) {
+            y -= speed;
+        }
+    }
+
+    public void moveDown() {
+        // 確保玩家不會超過視窗下邊界，並根據玩家ID限制區域
+        if (y + height + speed <= PVPmode.height && (id == 2 || y + height + speed <= PVPmode.height / 2)) {
+            y += speed;
+        }
+    }
+
+    public void moveLeft() {
+        // 確保玩家不會超過視窗左邊界
+        if (x - speed >= 0) {
+            x -= speed;
+        }
+    }
+
+    public void moveRight() {
+        // 確保玩家不會超過視窗右邊界
+        if (x + width + speed <= PVPmode.width) {
+            x += speed;
+        }
+    }
+
+    @Override
+    public Rectangle getRec() {
+        return new Rectangle(x, y, width, height);
+    }
+}
