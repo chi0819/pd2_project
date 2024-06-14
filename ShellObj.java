cf3c8d0efce8d92aa53ff219abdbaebf1202d9a2
ethan8190 <eason8190@gmail.com>
2024-06-14 17:42:17 +0800

    add PVPmode

--- ShellObj.java
+++ ShellObj.java
@@ -9,7 +9,9 @@ import java.awt.Font;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 
-public class ShellObj extends GameObj{
+public class ShellObj extends GameObj {
+    public int playerId; // 發射子彈的玩家ID
+
     @Override
     public Image getImage() {
         return super.getImage();
@@ -19,21 +21,27 @@ public class ShellObj extends GameObj{
         super();
     }
 
-    public ShellObj(Image img,int x,int y,int width,int height,double speed,Planewar frame) {
-        super(img,x,y,width,height,speed,frame);
+    public ShellObj(Image img, int x, int y, int width, int height, double speed, Planewar frame) {
+        super(img, x, y, width, height, speed, frame);
+    }
+
+    public ShellObj(Image img, int x, int y, int width, int height, int speed, Planewar frame, int playerId) {
+        super(img, x, y, width, height, speed, frame);
+        this.playerId = playerId; // 設置子彈來源玩家ID
     }
 
     public void paintSelf(Graphics gImage) {
         super.paintSelf(gImage);
         y -= speed;
-        if (y < 0) { //out of the frame
+        // If shell out of screen, remove it
+        if (y < 0 || y > PVPmode.height) { // out of the frame
             this.x = -100;
             this.y = 100;
             GameUtil.removeList.add(this);
         }
     }
-    
+
     public Rectangle getRec() {
-        return new Rectangle(x,y,width,height);
+        return new Rectangle(x, y, width, height);
     }
 }
